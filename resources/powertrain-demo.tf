07f6ad9142504319a1a17d3a209faba0a9b57ebe
Grigorii Osipov <grigorii.osipov@dataminded.com>
2024-08-23 14:27:06 +0200

    init commit

---
+++ resources/powertrain-demo.tf
@@ -0,0 +1,27 @@
+locals {
+  project_name = "powertrain-demo"
+  uuid_pattern = "????????-????-????-????-????????????"
+}
+
+resource "aws_iam_role" "default" {
+  name               = "${local.project_name}-${var.env_name}"
+  assume_role_policy = data.aws_iam_policy_document.default.json
+}
+
+data "aws_iam_policy_document" "default" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringLike"
+      variable = "${replace(var.aws_iam_openid_connect_provider_url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:${var.env_name}:${replace(local.project_name, "_", ".")}-${local.uuid_pattern}"]
+    }
+
+    principals {
+      identifiers = [var.aws_iam_openid_connect_provider_arn]
+      type        = "Federated"
+    }
+  }
+}
